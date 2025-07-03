a3a6c291d113cce5dffef5a169384eaf6095a7fd
Racim Saal <racimsaal@gmail.com>
2025-07-03 22:48:55 +0100

    finish footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { FOOTER_LINKS } from '@/constants'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
 import { link } from 'fs'
 
 
 const Footer = () => {
     return (
-        <section className='flexCenter mb-24'>
-            <div className='border-2 border-red-500 padding-container max-container flex w-full flex-col gap-14'>
-                <div className='border-2 border-blue-500 flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+        <footer className='flexCenter mb-24'>
+            <div className='padding-container max-container flex w-full flex-col gap-14'>
+                <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
                     <Link href={'/'} className='mb-10'>
                         <Image src={'hilink-logo.svg'} alt='logo' width={74} height={29} />
                     </Link>
@@ -27,14 +27,44 @@ const Footer = () => {
                             </FooterColumn>
                         ))}
 
-                        <div className='flex flex'></div>
+                        <div className='flex flex-col gap-5'>
+                            <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                                {FOOTER_CONTACT_INFO.links.map((link) => (
+                                    <Link
+                                        href={'/'}
+                                        key={link.label}
+                                        className='flex gap-4 md:flex-col lg:flex-row'
+                                    >
+                                        <p className='whitespace-nowrap'>
+                                            {link.label}:
+                                        </p>
+                                        <p className='medium-14 whitespace-nowrap text-blue-70'>{link.value}</p>
+                                    </Link>
+                                ))}
+                            </FooterColumn>
+                        </div>
 
+                        <div className='flex flex-col gap-5'>
+                            <FooterColumn title={SOCIALS.title}>
+                                <ul className='regular-14 flex gap-4 text-gray-30'>
+                                    {SOCIALS.links.map((link) => (
+                                        <Link key={link} href={'/'}>
+                                            <Image src={link}
+                                                alt='logo'
+                                                width={24}
+                                                height={24} />
+                                        </Link>
+                                    ))}
+                                </ul>
+                            </FooterColumn>
+                        </div>
 
                     </div>
-
                 </div>
+                <div className='border bg-gray-20' />
+                <p className='regular-14 w-full text-center text-gray-30'>{new Date().getFullYear()} Hilink | All rights reserved</p>
             </div>
-        </section>
+        </footer>
     )
 }
 
